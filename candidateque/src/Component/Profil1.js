d06dc9ae5ea5e8f0f39982d4bb4e10e5162006f6
Dislaire Frédéric <dislaire.frederic@hotmail.fr>
2021-01-28 17:33:01 +0100

    formulaire react

---
+++ candidateque/src/Component/Profil1.js
@@ -0,0 +1,118 @@
+
+import React, { useState } from'react'
+import {Row,Col, Container,Form,Button} from 'react-bootstrap'
+import Souhait from './souhait';
+
+
+function Profil1(props) {
+
+  const [metier,setMetier]=useState([]);
+  const [experience, setExperience]=useState([]);
+  const [poinCles,setPointCle]=useState([]);
+  
+
+  
+
+
+console.log(props.numeroPage);
+
+if (props.numeroPage === 1){
+  return (
+    
+    <div>
+   <Container>
+    <Row>
+   <Col>
+
+<Form>
+  <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Métier souhaité</Form.Label>
+    <Form.Control type="text" placeholder="Exemple : Développeur web"/>
+  </Form.Group>
+
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>Expérience dans le métier</Form.Label>
+    <Form.Control as="select" onChange={evt=>setMetier(evt)}>
+      <option selected>Précisez la durée</option>
+      <option>Débutant</option>
+      <option>1 an d'expérience</option>
+      <option>2 ans d'expérience</option>      
+      <option>3 ans d'expérience</option>
+      <option>4 ans d'expérience</option>
+      <option>5 ans d'expérience</option>
+      <option value={1}>6 ans d'expérience</option>
+      <option>7 ans d'expérience</option>
+      <option>8 ans d'expérience</option>
+      <option>9 ans d'expérience</option>
+      <option>10 ans et + </option>
+    </Form.Control>
+  </Form.Group>
+
+  <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Points clés</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>Disponibilité</Form.Label>
+    <Form.Control as="select">
+      <option selected>Précisez vos Disponibilité</option>
+      <option>Immédiatement</option>
+      <option>Moins de 3 mois</option>
+      <option>Entre 3 et 6 mois</option>      
+      <option>Plus de 6 mois</option>
+      
+    </Form.Control>
+  </Form.Group>
+
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>Mobilité</Form.Label>
+    <Form.Control as="select">
+      <option selected>Précisez vos Mobilités</option>
+      <option>Département</option>
+      <option>Région</option>
+      <option>France Entière</option>      
+      <option>Europe</option>
+      <option>Reste du monde</option>
+      
+    </Form.Control>
+  </Form.Group>
+
+  <Button  type='submit' onClick={()=>props.onChangePage(2)} variant="primary">Suivant</Button>
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+</Form>
+   
+   
+   </Col>
+        
+       </Row>
+       </Container>
+    
+
+    </div>
+  );
+
+  }
+  else if (props.numeroPage === 2){
+    return (
+
+      <Souhait/>
+      
+    );
+  }
+}
+
+export default Profil1;
+
--- candidateque/src/index.js
+++ candidateque/src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Profil1 from './Profil1';
+import {Row,Col, Container}from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Profil1 from './Component/Profil1'
 
+function App (props) {
+
+  const [page,setPage]=useState(1);
+
+  const handleChangePage =(value)=>{
+      setPage(value);
+  }
+ 
+  useEffect(()=>{
+
+  },[page]);
+
+
+return(
+  <Container>
+     <Row>
+    <Col>navbar</Col>
+  </Row>
+  <Row>
+    <Col>
+    <Profil1 
+    onChangePage={(value)=>handleChangePage(value)}
+    numeroPage={page}
+    />
+    </Col>
+  </Row>
+</Container>
+)
+
+}
 
 ReactDOM.render(
-    <Profil1 />,
+    <App />,
   document.getElementById('root')
 );
 
