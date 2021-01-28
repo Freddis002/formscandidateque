a6e967ef1acf4051fa96ceda2fc551c8ddc798e5
Dislaire Frédéric <dislaire.frederic@hotmail.fr>
2021-01-28 11:27:49 +0100

    react

---
+++ candidateque/src/Profil1.js
@@ -0,0 +1,23 @@
+
+function Profil1() {
+  return (
+    <div>
+     <select class="form-control" id="exampleFormControlSelect1" required>
+          <option selected>Précisez la durée</option>
+          <option>Débutant</option>
+          <option>1 an d'expérience</option>
+          <option>2 ans d'expérience</option>
+          <option>3 ans d'expérience</option>
+          <option>4 ans d'expérience</option>
+          <option>5 ans d'expérience</option>
+          <option>6 ans d'expérience</option>
+          <option>7 ans d'expérience</option>
+          <option>8 ans d'expérience</option>
+          <option>9 ans d'expérience</option>
+          <option>10 ans d'expérience et +</option>
+        </select>
+    </div>
+  );
+}
+
+export default Profil1;
---
+++ candidateque/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Profil1 from './Profil1';
+
+
+ReactDOM.render(
+    <Profil1 />,
+  document.getElementById('root')
+);
+
